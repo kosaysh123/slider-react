f3ebe57de0a20bb480f4298cb9c9b5cb49899661
kosaysh123 <qosaish703@gmail.com>
2023-11-11 22:51:50 +0300

    slider react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  const App = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slides = [
+      'Slide 1',
+      'Slide 2',
+      'Slide 3',
+      'Slide 4'
+    ];
+  
+    const nextSlide = () => {
+      setCurrentSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
+    };
+  
+    const prevSlide = () => {
+      setCurrentSlide((prevSlide) => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1));
+    };
+
+
+    
+      return (
+        <div className="slider-container">
+          <button onClick={prevSlide}>Previous</button>
+          <div className="slide">{slides[currentSlide]}</div>
+          <button onClick={nextSlide}>Next</button>
+        </div>
+      );
+    };
+  
+
 
 export default App;
